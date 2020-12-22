021f56fa87ebc9e0794989638fdcf9566536b1f9
王杰 <jie.wang@magicpie.com>
2020-12-22 14:38:21 +0800

    v1.4.4

---
+++ build/publish.js
@@ -0,0 +1,34 @@
+const fs = require('fs-extra')
+const Path = require('path')
+const childProcess = require('child_process')
+
+const projectPath = Path.join(__dirname, '../projects')
+const dirs = fs.readdirSync(projectPath)
+
+const version = '1.4.4'
+
+for (const dir of dirs) {
+    const projectBasePath = Path.join(projectPath, dir)
+    const packageJsonPath = Path.join(projectBasePath, 'package.json')
+    const pkg = fs.readJsonSync(packageJsonPath)
+    pkg.version = version
+    fs.writeJsonSync(packageJsonPath, pkg, {spaces: 4})
+
+
+    let cmd = ''
+    if (pkg.scripts.build) {
+        cmd += 'npm run build &&'
+    }
+    cmd += 'npm publish --access=public'
+    const child = childProcess.exec(cmd, {
+        cwd: projectBasePath
+    })
+
+    child.stdout.addListener('data', (data) => {
+        console.log(data.toString())
+    })
+
+    child.stderr.addListener('data', (data) => {
+        console.error(data.toString())
+    })
+}
